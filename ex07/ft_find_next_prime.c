f6195c66db979d678cdc8742e31f3e5a44a61445
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-11 11:29:45 +0800

    added ex06.

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -1,32 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/11 09:51:02 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/11 11:29:03 by pteh             ###   ########.fr       */
+/*   Created: 2023/08/11 10:59:21 by pteh              #+#    #+#             */
+/*   Updated: 2023/08/11 11:22:04 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include <stdio.h>
 #include <limits.h>
 
-int	ft_is_prime(int nb)
+int	ft_find_next_prime(int nb)
 {
 	int	sqrt;
+	int	upper_limit;
 
 	sqrt = 2;
+	upper_limit = 46341;
 	if (nb < 0 || nb == 0 || nb == 1 || nb > INT_MAX)
 		return (0);
 	if (nb > 2)
 	{
-		while (nb < 46340 && sqrt * sqrt <= nb)
+		while (nb < upper_limit && sqrt * sqrt <= upper_limit) //while we increase towards
 		{
-			if (nb % sqrt == 0)
-				return (0);
 			sqrt++;
+			if (nb % sqrt != 0)
+				return (nb);
 		}
 	}
 	return (1);
@@ -34,9 +37,8 @@ int	ft_is_prime(int nb)
 
 int	main(void)
 {
-	printf("is 5 a prime?:%d\n", ft_is_prime(5));
-	printf("is 6 a prime?:%d\n", ft_is_prime(6));
-	printf("is 7 a prime?:%d\n", ft_is_prime(7));
-	printf("is 8 a prime?:%d\n", ft_is_prime(8));
-	printf("is 859 a prime?:%d\n", ft_is_prime(859));
+	printf("is 5 a prime?:%d\n", ft_find_next_prime(5));
+	printf("is 6 a prime?:%d\n", ft_find_next_prime(6));
+	printf("is 7 a prime?:%d\n", ft_find_next_prime(7));
+	printf("is 8 a prime?:%d\n", ft_find_next_prime(8));
 }
